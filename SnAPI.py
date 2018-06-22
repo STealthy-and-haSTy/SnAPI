1a05ad5ca6a700512dd39fb6502954986ea73642
Terence Martin <github@nurdz.com>
2018-06-21 21:49:20 -0700

    Enhance the SnAPI lookup command
    This enhances the SnAPI lookup command so that it works the same way
    in build files as it does in plugins; the word under the cursor is
    used as the initial topic, and if it is a topic in the index, that
    topic is opened directly.
    
    This also includes the notion of a default package to display the
    index for in cases where the current file is not a plugin or a build
    file, which is currently hard coded to be the root topic in the
    SublimeAPI help package.
    
    This should probably at some level be configurable by the user.

--- SnAPI.py
+++ SnAPI.py
@@ -16,37 +16,42 @@ class SnapiLookupCommand(sublime_plugin.TextCommand):
     entries.
     """
     def run(self, edit):
-        # Jump directly to the build system documentation in builds.
-        if self.is_build():
+        # For unknown files, open the default topic for a default package.
+        #
+        # TODO: Should be user configurable?
+        if not self.is_build() and not self.is_plugin():
+            return sublime.run_command(
+                "hyperhelp_topic", {"package": "SublimeAPI"})
+
+        # Get the help index for the type of file currently focused.
+        if self.is_plugin():
+            pkg_info = help_index_list().get("SublimeAPI")
+        else:
+            pkg_info = help_index_list().get("SublimeBuild")
+
+        # Get an initial topic; the selected text or the word under the cursor.
+        extract = self.view.sel()[0]
+        if extract.empty():
+            extract = self.view.expand_by_class(extract,
+                sublime.CLASS_WORD_START | sublime.CLASS_WORD_END)
+        topic = self.view.substr(extract)
+
+        # If the initial topic exists, open it directly.
+        if lookup_help_topic(pkg_info, topic):
             return sublime.run_command("hyperhelp_topic", {
-                "package": "SublimeBuild",
-                "topic": "index.txt"
+                "package": pkg_info.package,
+                "topic": topic
             })
 
-        # In plugin files, try to zero in on the topic under the cursor or in
-        # the selection.
-        topic = None
-        if self.is_plugin():
-            extract = self.view.sel()[0]
-            if extract.empty():
-                extract = self.view.expand_by_class(extract,
-                    sublime.CLASS_WORD_START | sublime.CLASS_WORD_END)
-            topic = self.view.substr(extract)
-
-            pkg_info = help_index_list().get("SublimeAPI")
-            if lookup_help_topic(pkg_info, topic):
-                return sublime.run_command("hyperhelp_topic", {
-                    "package": "SublimeAPI",
-                    "topic": topic
-                })
-
-        # Open the API help by default, optionally also using the topic
-        # selected above.
-        sublime.run_command("hyperhelp_index", {"package": "SublimeAPI"})
+        # Topic is unknown; open the help index for the package and insert the
+        # topic as an initial filter.
+        sublime.run_command("hyperhelp_index", {"package": pkg_info.package})
         if topic is not None:
             self.view.window().run_command("insert", {"characters": topic})
 
+
     def is_plugin(self):
+        # Could also ensure the file exists in a Package.
         return self.view.match_selector(0, "source.python")
 
     def is_build(self):
@@ -57,10 +62,11 @@ class SnapiLookupCommand(sublime_plugin.TextCommand):
         return name.endswith(".sublime-build")
 
     def is_enabled(self):
-        return all(key in help_index_list() for key in (
-            "SublimeAPI",
-            "SublimeBuild"
-        ))
+        if self.is_build():
+            return "SublimeBuild" in help_index_list()
+
+        # For plugins and the default case
+        return "SublimeAPI" in help_index_list()
 
 
 ###----------------------------------------------------------------------------
